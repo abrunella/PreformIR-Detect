5897106324e18c71bb5692e2e1c2b857b6ca3def
abrunella <adam.brunella@gmail.com>
2019-10-08 06:45:02 -0700

    Image annotations

--- resize_images.py
+++ resize_images.py
@@ -24,9 +24,9 @@ if __name__ == "__main__":
         "--ext", help="Raw image files extension to resize.", default="jpg", type=str
     )
     parser.add_argument(
-        "--target-size",
-        help="Target size to resize as a tuple of 2 integers.",
-        default="(800, 600)",
+        "--target-scale",
+        help="Target size to resize as a percentage.",
+        default="25",
         type=str,
     )
     args = parser.parse_args()
@@ -34,9 +34,9 @@ if __name__ == "__main__":
     raw_dir = args.raw_dir
     save_dir = args.save_dir
     ext = args.ext
-    target_size = eval(args.target_size)
-    msg = "--target-size must be a tuple of 2 integers"
-    assert isinstance(target_size, tuple) and len(target_size) == 2, msg
+    target_size = args.target_scale
+    #msg = "--target-size must be a tuple of 2 integers"
+    #assert isinstance(target_size, tuple) and len(target_size) == 2, msg
     fnames = glob.glob(os.path.join(raw_dir, "*.{}".format(ext)))
     os.makedirs(save_dir, exist_ok=True)
     print(
@@ -44,15 +44,22 @@ if __name__ == "__main__":
             len(fnames), raw_dir, target_size
         )
     )
+
     for i, fname in enumerate(fnames):
         print(".", end="", flush=True)
-        img = cv2.imread(fname)
-        img_small = cv2.resize(img, target_size)
+        img = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
+        width = int(img.shape[1] * int(target_size) / 100)
+        height = int(img.shape[0] * int(target_size) / 100)
+        dim = (width, height)
+        img_small = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
         new_fname = "{}.{}".format(str(i), ext)
         small_fname = os.path.join(save_dir, new_fname)
         cv2.imwrite(small_fname, img_small)
+    
     print(
         "\nDone resizing {} files.\nSaved to directory: `{}`".format(
             len(fnames), save_dir
         )
     )
+
+
